9a4e1175b8fbd2b4b4bff27f3968f293e12a667e
bian-weikun <bian-weikun@smtp.telek.com.cn>
2021-07-08 11:47:42 +0800

    提交

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './krouter'
+// import store from './kstore'
+// import store from './store'
+// import router from './router'
+
+Vue.config.productionTip = false
+// 事件总线
+Vue.prototype.$bus = new Vue()
+
+new Vue({
+  router,
+
+  // store,
+  render: h => h(App)
+}).$mount('#app')
