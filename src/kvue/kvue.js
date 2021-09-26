2e0e7fae77dca1810273c136d3836872dca927da
Dreamer <504478089@qq.com>
2021-09-26 23:44:13 +0800

    提交

--- src/kvue/kvue.js
+++ src/kvue/kvue.js
@@ -10,6 +10,9 @@ function defineReactive(obj,key,val){
         get(){
             // console.log('get--',val)
             Dep.target&&dep.addDep(Dep.target)
+            if (Array.isArray(val)) {
+                dependArray(val)
+            }
             return val
         },
         set(newVal){
@@ -23,6 +26,17 @@ function defineReactive(obj,key,val){
     })
 }
 
+function dependArray (value) {
+    for (var e = (void 0), i = 0, l = value.length; i < l; i++) {
+      e = value[i];
+    //   e && e.__ob__ && e.__ob__.dep.addDep();
+    Dep.target&&dep.addDep(Dep.target)
+      if (Array.isArray(e)) {
+        dependArray(e);
+      }
+    }
+  }
+
 function observe(obj){
     if(typeof obj  !=='object'||obj===null){
         return
@@ -33,38 +47,125 @@ function observe(obj){
     // });
     new Observe(obj)
 }
+const orginalArray = Array.prototype
+var arrayMethods = Object.create(orginalArray);
+var methodsToPatch = [
+    'push',
+    'pop',
+    'shift',
+    'unshift',
+    'splice',
+    'sort',
+    'reverse'
+  ];
+  methodsToPatch.forEach(function (method) {
+    var original = orginalArray[method];
+    // arrayMethods[method] = function(){
+    //     var args = [], len = arguments.length;
+    //     while ( len-- ) args[ len ] = arguments[ len ];
+    //     original.apply(this, args);
+    //     var ob = this.__ob__;
+    //     var inserted;
+    //     switch (method) {
+    //         case 'push':
+    //         case 'unshift':
+    //             inserted = args;
+    //             break
+    //         case 'splice':
+    //             inserted = args.slice(2);
+    //             break
+    //     }
+        
+    //     if (inserted) { ob.observeArray(inserted); }
+    //     const dep = new Dep()
+    //     dep.notify();
+    // }
+    def(arrayMethods, method, function mutator () {
+        var args = [], len = arguments.length;
+        while ( len-- ) args[ len ] = arguments[ len ];
+        console.log(this,arguments)
+        var result = original.apply(this, args);
+        var ob = this.__ob__;
+        var inserted;
+        switch (method) {
+          case 'push':
+          case 'unshift':
+            inserted = args;
+            break
+          case 'splice':
+            inserted = args.slice(2);
+            break
+        }
+        if (inserted) { ob.observeArray(inserted); }
+        // notify change
+        ob.dep.notify();
+        return result
+      });
+    
+})
+
+function def (obj, key, val, enumerable) {
+    Object.defineProperty(obj, key, {
+      value: val,
+      enumerable: !!enumerable,
+      writable: true,
+      configurable: true
+    });
+  }
+  var hasProto = '__proto__' in {};
+  var arrayKeys = Object.getOwnPropertyNames(arrayMethods);
+
+  
+  /**
+   * Augment a target Object or Array by intercepting
+   * the prototype chain using __proto__
+   */
+   function protoAugment (target, src) {
+    /* eslint-disable no-proto */
+    target.__proto__ = src;
+    /* eslint-enable no-proto */
+  }
+
+  /**
+   * Augment a target Object or Array by defining
+   * hidden properties.
+   */
+  /* istanbul ignore next */
+  function copyAugment (target, src, keys) {
+    for (var i = 0, l = keys.length; i < l; i++) {
+      var key = keys[i];
+      def(target, key, src[key]);
+    }
+  }
 
 class Observe{
     constructor(value) {
         this.value = value
-        this.walk(value)
+        this.dep = new Dep();
+        def(value, '__ob__', this);
+        if(Array.isArray(value)){
+            // if (hasProto) {
+            //     obj.__proto__  = arrayMethods
+            //   } else {
+            //     copyAugment(obj, arrayMethods, arrayKeys);
+            //   }
+            value.__proto__  = arrayMethods
+            this.observeArray(value);
+        }else{
+            this.walk(value)
+        }
+        
 
     }
     walk(obj){
-        const dep = new Dep()
-        const orginalArray = Array.prototype
-        var creatArray = Object.create(orginalArray);
-        ['push','pop','shift','unshift'].forEach((method)=>{
-            creatArray[method] = function(){
-                console.log(this,arguments)
-                orginalArray[method].apply(this,arguments)
-            
-                observe(this)
-                // val = newVal
-                dep.notify()
-            }
-        })
-        if(Array.isArray(obj)){
-            obj._proto_ = creatArray
-            obj.prototype = creatArray
-            const keys = Object.keys(obj)
-            for(let i =0;i<obj.length;i++){
-                observe(obj[i])
-            }
-        }else{
-            Object.keys(obj).forEach(item => {
-                defineReactive(obj,item,obj[item])
-            });
+        Object.keys(obj).forEach(item => {
+            defineReactive(obj,item,obj[item])
+        });
+        
+    }
+    observeArray (items) {
+        for (var i = 0, l = items.length; i < l; i++) {
+            observe(items[i]);
         }
     }
 }
